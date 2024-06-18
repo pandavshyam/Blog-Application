3099076f7b65c0540844c5ed1203420152468894
Shyam Pandav <pandavshyam@gmail.com>
2024-06-19 00:18:38 +0530

    1. Improve models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,18 @@
+# frozen_string_literal: true
+
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
   has_many :blogs, class_name: 'Blog', inverse_of: :author, dependent: :destroy
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  ## Database authenticatable
   field :email,              type: String, default: ""
   field :encrypted_password, type: String, default: ""
 
-  ## Recoverable
   field :reset_password_token,   type: String
   field :reset_password_sent_at, type: Time
 
-  ## Rememberable
   field :remember_created_at, type: Time
 end
