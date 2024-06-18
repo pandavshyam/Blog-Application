0650fe55fa0473c6d7f34b709ab11e9374d0229d
Shyam Pandav <pandavshyam@gmail.com>
2024-06-18 23:50:52 +0530

    1. Use respond_to for safety reasons

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,45 +10,73 @@ class BlogsController < ApplicationController
       else
         pagy_cursor(Blog.where(status: 'published').includes(:author))
       end
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @blogs }
+    end
   end
 
   # GET /blogs/1
   def show
     @comment = @blog.comments.build
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @blog }
+    end
   end
 
   # GET /blogs/new
   def new
     @blog = current_user.blogs.build
+
+    respond_to do |format|
+      format.html # new.html.erb
+    end
   end
 
   # POST /blogs
   def create
     @blog = current_user.blogs.build(blog_params)
 
-    if @blog.save
-      redirect_to @blog, notice: 'Blog was successfully created.'
-    else
-      render :new, status: :unprocessable_entity
+    respond_to do |format|
+      if @blog.save
+        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+        format.json { render json: @blog, status: :created, location: @blog }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # GET /blogs/1/edit
-  def edit; end
+  def edit
+    respond_to do |format|
+      format.html # edit.html.erb
+    end
+  end
 
   # PATCH/PUT /blogs/1
   def update
-    if @blog.update(blog_params)
-      redirect_to @blog, notice: 'Blog was successfully updated.'
-    else
-      render :edit, status: :unprocessable_entity
+    respond_to do |format|
+      if @blog.update(blog_params)
+        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+        format.json { render json: @blog, status: :ok, location: @blog }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # DELETE /blogs/1
   def destroy
     @blog.destroy
-    redirect_to blogs_url, notice: 'Blog was successfully destroyed.'
+    respond_to do |format|
+      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
